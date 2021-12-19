4c37ae474fde506123e5ad8e31175d830cc34575
ntalbs <ntalbsen@gmail.com>
2021-12-19 14:27:29 +0000

    Add message while starting server

--- src/main.rs
+++ src/main.rs
@@ -119,7 +119,10 @@ async fn echo(req: HttpRequest, body: String) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(echo))
+    let server = HttpServer::new(|| App::new().service(echo));
+    println!("Serving on http://localhost:8080");
+
+    server
         .bind("127.0.0.1:8080")?
         .run()
         .await
